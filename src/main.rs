0ea8562c38e4afa2d694231daa6e59caa44b3dc7
deepso <deeps.o7o8.y@gmail.com>
2024-06-03 16:45:04 +0530

    add loop for wrong statements

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
     // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim())
+    print!("$ ");
+    io::stdout().flush().unwrap();
+
+    while stdin.read_line(&mut input).is_ok() {
+        println!("{}: command not found", input.strip_suffix("\n").unwrap());
+        input.clear();
+        print!("$ ");
+        io::stdout().flush().unwrap();
+    }
 }
