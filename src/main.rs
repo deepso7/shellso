634cf51e55a2d283715cb741096468bd846e9f15
deepso <deeps.o7o8.y@gmail.com>
2024-06-03 16:51:58 +0530

    handle exit 0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,7 +14,12 @@ fn main() {
     io::stdout().flush().unwrap();
 
     while stdin.read_line(&mut input).is_ok() {
-        println!("{}: command not found", input.strip_suffix("\n").unwrap());
+        match input.trim().to_lowercase().as_str() {
+            "exit 0" => exit(0),
+
+            _ => println!("{}: command not found", input.strip_suffix("\n").unwrap()),
+        }
+
         input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
